c061b2c3a2668327b681ed449bc605dd78ebcad6
Paolo Razon <plgrazon@gmail.com>
2024-08-26 20:25:38 -0400

    use right reducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,5 +1,5 @@
 import {combineReducers} from '@reduxjs/toolkit';
-import {counterSlice} from '../features/counter/counterSlice';
+import counterSlice from '../features/counter/counterSlice';
 
 export const rootReducer = combineReducers({
   counter: counterSlice,
