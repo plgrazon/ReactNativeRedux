1c990003effdfa7eb349de78ba156587d947e52c
Paolo Razon <plgrazon@gmail.com>
2024-08-26 20:26:39 -0400

    update counter component

--- src/features/counter/counter.tsx
+++ src/features/counter/counter.tsx
@@ -1,13 +1,33 @@
 import {useState} from 'react';
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
-import {counterSlice} from './counterSlice';
+import {
+  decrement,
+  decrementIfOdd,
+  increment,
+  incrementAsync,
+  incrementIfOdd,
+} from './counterSlice';
+import {selectCount, selectStatus} from './counterSelector';
+import {useAppDispatch, useAppSelector} from '../../store/hook';
+import AppModal from '../modal/AppModal';
 
 const Counter = () => {
   const [inputValue, setInputValue] = useState<string>('');
+  const [visible, setIsVisible] = useState<boolean>(false);
+
+  const toggleModal = () => setIsVisible(!visible);
+
+  const dispatch = useAppDispatch();
+  const count = useAppSelector(selectCount);
+  const status = useAppSelector(selectStatus);
+
+  const isOdd = count % 2 !== 0;
+  const defaultInputValue = inputValue ? Number(inputValue) : 1;
 
   return (
     <View style={styles.parentView}>
-      <Text>{`Current value: ${inputValue}`}</Text>
+      <Text>{`Current status: ${status}`}</Text>
+      <Text>{`Current value: ${count}`}</Text>
       <View style={styles.childView}>
         <TextInput
           style={styles.textInput}
@@ -18,8 +38,31 @@ const Counter = () => {
           keyboardType="numeric"
         />
       </View>
-      <Button title="increment" />
-      <Button title="decrement" />
+      <Button title="+1" onPress={() => dispatch(increment())} />
+      <Button title="-1" onPress={() => dispatch(decrement())} />
+      <Button
+        title="increment if odd"
+        onPress={() => {
+          if (isOdd) dispatch(incrementIfOdd(defaultInputValue));
+          else setIsVisible(true);
+        }}
+      />
+      <Button
+        title="decrement if odd"
+        onPress={() => {
+          if (isOdd) dispatch(decrementIfOdd(defaultInputValue));
+          else setIsVisible(true);
+        }}
+      />
+      <Button
+        title="async increment"
+        onPress={() => dispatch(incrementAsync(defaultInputValue))}
+      />
+      <AppModal
+        visible={visible}
+        toggleModal={toggleModal}
+        displayText="Number is Even"
+      />
     </View>
   );
 };
