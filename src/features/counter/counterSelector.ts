423bed75e8a45d7d091a91d87f64c6ef54544946
Paolo Razon <plgrazon@gmail.com>
2024-08-26 20:26:10 -0400

    update selector

--- src/features/counter/counterSelector.ts
+++ src/features/counter/counterSelector.ts
@@ -1,4 +1,4 @@
 import {RootState} from '../../store/store';
 
 export const selectCount = (state: RootState) => state.counter.value;
-export const selectStatus = (state: RootState) => state.counter.value;
+export const selectStatus = (state: RootState) => state.counter.status;
