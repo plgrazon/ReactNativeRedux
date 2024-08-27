67f42d5f09dca98feb73466b266d5f6bbc8fe7f6
Paolo Razon <plgrazon@gmail.com>
2024-08-26 20:25:17 -0400

    add reducers

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -27,6 +27,9 @@ export const counterSlice = createSlice({
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     },
+    decrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value -= action.payload;
+    },
   },
   extraReducers: builder => {
     builder
@@ -44,8 +47,13 @@ export const counterSlice = createSlice({
 });
 
 export default counterSlice.reducer;
-export const {increment, decrement, incrementByAmount} = counterSlice.actions;
+export const {increment, decrement, incrementByAmount, decrementByAmount} =
+  counterSlice.actions;
 
+// The function below is called a thunk, which can contain both sync and async logic
+// that has access to both `dispatch` and `getState`. They can be dispatched like
+// a regular action: `dispatch(incrementIfOdd(10))`.
+// Here's an example of conditionally dispatching actions based on current state.
 export const incrementIfOdd = (amount: number): AppThunk => {
   return (dispatch, getState) => {
     const currentValue = selectCount(getState());
@@ -53,10 +61,24 @@ export const incrementIfOdd = (amount: number): AppThunk => {
   };
 };
 
+export const decrementIfOdd = (amount: number): AppThunk => {
+  return (dispatch, getState) => {
+    const currentValue = selectCount(getState());
+    if (currentValue % 2 === 1) dispatch(decrementByAmount(amount));
+  };
+};
+
+// Thunks are commonly used for async logic like fetching data.
+// The `createAsyncThunk` method is used to generate thunks that
+// dispatch pending/fulfilled/rejected actions based on a promise.
+// In this example, we make a mock async request and return the result.
+// The `createSlice.extraReducers` field can handle these actions
+// and update the state with the results.
 export const incrementAsync = createAsyncThunk(
   'counter/fetchCount',
   async (amount: number) => {
     const response = await fetchCount(amount);
+    // The value we return becomes the `fulfilled` action payload
     return response.data;
   },
 );
