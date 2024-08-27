63e49d0e958653d74828364599f3c3f5c61f4f21
Paolo Razon <plgrazon@gmail.com>
2024-08-26 20:23:27 -0400

    add reject

--- src/features/counter/counterApi.ts
+++ src/features/counter/counterApi.ts
@@ -1,6 +1,13 @@
 // A mock function to mimic making an async request for data
 export const fetchCount = (amount = 1) => {
-  return new Promise<{data: number}>(resolve =>
-    setTimeout(() => resolve({data: amount}), 500),
+  const timeOutDuration = [500, 1000, 2000, 4000, 8000];
+  const timeOut =
+    timeOutDuration[Math.floor(Math.random() * timeOutDuration.length)];
+
+  return new Promise<{data: number}>((resolve, reject) =>
+    setTimeout(() => {
+      if (Math.floor(Math.random() * 2)) reject();
+      resolve({data: amount});
+    }, timeOut),
   );
 };
