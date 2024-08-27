416a970e72365ec86eaad5f9e71eeaff9e03e36c
Paolo Razon <plgrazon@gmail.com>
2024-08-27 08:46:52 -0400

    update api

--- src/features/counter/counterApi.ts
+++ src/features/counter/counterApi.ts
@@ -1,13 +1,16 @@
 // A mock function to mimic making an async request for data
 export const fetchCount = (amount = 1) => {
-  const timeOutDuration = [500, 1000, 2000, 4000, 8000];
+  const timeOutDuration = [500, 1000, 2000, 4000];
   const timeOut =
     timeOutDuration[Math.floor(Math.random() * timeOutDuration.length)];
+  const rejectionRates = [0, 1, 1];
+  const rejectionProbabilty =
+    rejectionRates[Math.floor(Math.random() * rejectionRates.length)];
 
   return new Promise<{data: number}>((resolve, reject) =>
     setTimeout(() => {
-      if (Math.floor(Math.random() * 2)) reject();
-      resolve({data: amount});
+      if (rejectionProbabilty) reject();
+      else resolve({data: amount});
     }, timeOut),
   );
 };
